4cf891dc8199eff0d3d445fbe789aee1c433fecd
Alex Thomson <LXGaming@users.noreply.github.com>
2022-08-10 20:52:35 +1200

    go-health v1.0.0

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/LXGaming/go-health
+
+go 1.19
---
+++ health.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"net"
+	"os"
+)
+
+func main() {
+	address := flag.String("a", "127.0.0.1:4325", "address")
+	flag.Parse()
+
+	conn, err := net.Dial("tcp", *address)
+	if err != nil {
+		fmt.Println("dial error:", err)
+		os.Exit(1)
+	}
+
+	buf := make([]byte, 1)
+
+	n, err := io.ReadFull(conn, buf)
+	if err != nil {
+		fmt.Println("read error:", err)
+	}
+
+	err = conn.Close()
+	if err != nil {
+		fmt.Println("close error:", err)
+	}
+
+	if n == 1 && buf[0] == 0 {
+		os.Exit(0)
+	} else {
+		os.Exit(1)
+	}
+}
